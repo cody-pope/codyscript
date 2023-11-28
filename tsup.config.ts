f5c16170d6d4b626c0e7b55e818aaee2fe68046d
Cody Pope <cody.codypope.pope@gmail.com>
2023-11-28 00:40:10 -0500

    minify with terser instead of esbuild - it shaves off some size

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import type { Options } from "tsup";
 
 export const tsup: Options = {
   clean: true,
-  minify: true,
+  minify: "terser",
   outDir: "dist",
   target: "esnext",
   entry: ["src/index.ts"],
